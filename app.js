df4bb2e5688abfed4cd2bc4f1e6f709605dd3166
Joey Patterson <joeypatterson177@gmail.com>
2023-04-19 16:03:51 -0400

    link new pages to homepage buttons

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const damageRouter = require('./routes/damage');
+const weaponRouter = require('./routes/weapon');
+const handedRouter = require('./routes/handed');
 
 const app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', damageRouter);
+app.use('/', weaponRouter);
+app.use('/', handedRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const { getUsers } = require('../backend/controller');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.post('/', (req, res) => {
+  const { action } = req.body;
+  if (action === '/damage') {
+    res.redirect('/damage');
+  } else if (action === '/weapon') {
+    res.redirect('/weapon');
+  } else if (action === '/handedness') {
+    res.redirect('/handedness');
+  } else {
+    res.send('Invalid action');
+  }
 });
 
 module.exports = router;
